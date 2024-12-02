1ae7129646c35c23c7062e8b89acb5db85d2d783
Ryan Lynch <rynplynch@gmail.com>
2024-12-01 18:05:39 -0800

    import overlay for blender

--- overlays.nix
+++ overlays.nix
@@ -1,4 +1,5 @@
 {inputs, ...}: final: _prev: {
+  blender = _prev.blender ./pkgs/blender.nix {};
   mkGodot = _prev.callPackage ./pkgs/mkGodot.nix {};
   mkNixosPatch = _prev.callPackage ./pkgs/mkNixosPatch.nix {};
   export_templates = _prev.callPackage ./pkgs/export_templates.nix {};
