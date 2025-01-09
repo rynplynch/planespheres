1d6c2aacfe1528d1abd8d8750bbdaa10b078e596
Ryan Lynch <rynplynch@gmail.com>
2025-01-09 00:38:57 -0800

    We no longer use overlays to extend nixpkgs

--- overlays.nix
+++
@@ -1,5 +0,0 @@
-{inputs, ...}: final: _prev: {
-  mkGodot = _prev.callPackage ./pkgs/mkGodot.nix {};
-  mkNixosPatch = _prev.callPackage ./pkgs/mkNixosPatch.nix {};
-  export_templates = _prev.callPackage ./pkgs/export_templates.nix {};
-}
