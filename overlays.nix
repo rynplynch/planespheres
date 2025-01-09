04afa505e5c44fe722241c50207431c8ede1a292
Ryan Lynch <rynplynch@gmail.com>
2025-01-08 22:50:44 -0800

    No longer use blender to export models

--- overlays.nix
+++ overlays.nix
@@ -1,5 +1,4 @@
 {inputs, ...}: final: _prev: {
-  blender = _prev.blender ./pkgs/blender.nix {};
   mkGodot = _prev.callPackage ./pkgs/mkGodot.nix {};
   mkNixosPatch = _prev.callPackage ./pkgs/mkNixosPatch.nix {};
   export_templates = _prev.callPackage ./pkgs/export_templates.nix {};
