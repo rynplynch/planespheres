33ae2f5f6310df6639c9531f54761b22e02dbdd7
Ryan Lynch <rynplynch@gmail.com>
2025-02-07 12:32:13 -0800

    Build using default 'release' option

--- flake.nix
+++ flake.nix
@@ -103,7 +103,6 @@
           plane-spheres-materials-tar = inputs.plane-spheres-materials-tar;
           version = "1.0.0";
           pname = "index";
-          exportMode = "debug";
           src = ./game;
           preset = "Web"; # You need to create this preset in godot
         };
