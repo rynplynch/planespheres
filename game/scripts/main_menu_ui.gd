eb1a0685a4c216a4b9816c4733f07d3495535f4e
Ryan Lynch <rynplynch@gmail.com>
2025-02-16 23:14:55 -0800

    Remove nested file structure, new script, resource directories.

--- game/scripts/main_menu_ui.gd
+++ game/scripts/main_menu_ui.gd
@@ -1,14 +1,14 @@
 extends Control
 
 # scene swapped to when singleButton pushed
-@export_file("*.tscn") var lvl0Scene
+@export_file("*.tscn") var lvlDebug
 @export_file("*.tscn") var clientScene
 
 # button to trigger switch to single player scene
 @onready var lvl0_button = $"MarginContainer/VBoxContainer/SingleButton"
 
 func _on_single_button_pressed():
-	get_tree().change_scene_to_file(lvl0Scene)
+	get_tree().change_scene_to_file(lvlDebug)
 
 
 func _on_lobby_button_pressed():
