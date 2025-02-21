650b3c30f457694a82de987c1b93b29421dd4e69
Ryan Lynch <rynplynch@gmail.com>
2025-02-21 14:32:44 -0800

    Configure an additional db specifically for nakama.

--- services.nix
+++ services.nix
@@ -1,5 +1,12 @@
 {inputs}: {
   services.postgres."pg" = {
     enable = true;
+    initialScript.before = ''
+      CREATE DATABASE nakama;
+      \c nakama;
+      CREATE USER planespheres WITH password 'myserver';
+      GRANT ALL PRIVILEGES ON DATABASE nakama to planespheres;
+      GRANT USAGE, CREATE ON SCHEMA public TO planespheres;
+    '';
   };
 }
