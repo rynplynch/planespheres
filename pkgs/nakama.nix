31a6cdd92a99190272764cf29c8cd36b192d95d8
Ryan Lynch <rynplynch@gmail.com>
2025-02-20 19:29:16 -0800

    Starts the nakama server after initialization.

--- pkgs/nakama.nix
+++ pkgs/nakama.nix
@@ -21,6 +21,11 @@
           # this is checking the health of the postgres service found in services.nix
           depends_on."pg".condition = "process_healthy";
         };
+        nakama = {
+          command = "${lib.getExe cfg.package} --database.address ${config.services.postgres.pg.connectionURI {dbName = "planespheres";}}";
+          # only run after the init command is done
+          depends_on."init".condition = "process_completed_successfully";
+        };
       };
     };
 }
