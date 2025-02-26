65a9144580f74d532c58462a7dc0928601004e3e
Ryan Lynch <rynplynch@gmail.com>
2025-02-25 23:31:09 -0800

    Manually load UI on scene load.

--- game/scripts/networking_behavior.gd
+++ game/scripts/networking_behavior.gd
@@ -1,1 +1,9 @@
 extends Node
+
+@export_file("*.tscn") var networking_ui_path
+@onready var networking_ui : PackedScene = load(networking_ui_path)
+
+
+func _ready() -> void:
+		# Load main menu UI
+	self.add_child(networking_ui.instantiate())
