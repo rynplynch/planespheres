7828b911754112840e96c70f14e5807514c2ddf4
Ryan Lynch <rynplynch@gmail.com>
2025-03-08 12:19:08 -0800

    Register match creation RPC function.

--- data/modules/main.lua
+++ data/modules/main.lua
@@ -1,3 +1,6 @@
 local nk = require("nakama")
 local match = require("match")
 
+-- functions relating to match interaction
+-- create a match, sets ownerid to callers userid
+nk.register_rpc(create_match, "create_match")
