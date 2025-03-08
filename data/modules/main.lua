a8a25fe3787f3d007e665415b051da8da576bbb0
Ryan Lynch <rynplynch@gmail.com>
2025-03-08 12:19:22 -0800

    Register match listing RPC function.

--- data/modules/main.lua
+++ data/modules/main.lua
@@ -4,3 +4,6 @@ local match = require("match")
 -- functions relating to match interaction
 -- create a match, sets ownerid to callers userid
 nk.register_rpc(create_match, "create_match")
+
+-- returns matches based upon the json payload
+nk.register_rpc(get_matches, "get_matches")
