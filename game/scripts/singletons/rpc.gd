75979ba322aab836f8f56bb5be03d5cecaf8fe3e
Ryan Lynch <rynplynch@gmail.com>
2025-03-09 22:11:28 -0700

    Keep RPC functions inside their own script.

---
+++ game/scripts/singletons/rpc.gd
@@ -0,0 +1,9 @@
+extends Node
+
+# Ask the server to create a match for the user
+func create_match(logger : Control) -> bool:
+	return false
+
+# Ask the server to return a list of matches, based upon our request
+func get_matches(logger : Control) -> bool:
+	return false
