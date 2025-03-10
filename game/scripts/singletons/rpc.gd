260e9f32c76337f9959b02458fea969caff8b77e
Ryan Lynch <rynplynch@gmail.com>
2025-03-09 23:43:21 -0700

    Function that check if we can make an RPC call.

--- game/scripts/singletons/rpc.gd
+++ game/scripts/singletons/rpc.gd
@@ -7,3 +7,10 @@ func create_match(logger : Control) -> bool:
 # Ask the server to return a list of matches, based upon our request
 func get_matches(logger : Control) -> bool:
 	return false
+
+# Helper functions
+func _is_ready(client : NakamaClient, session : NakamaSession, logger : Control) -> bool:
+	# Make sure the user has all the parts they need to make a call
+	return await Networking.check_client_status(client, logger) \
+	&& Networking.check_session_status(session, logger) \
+	&& Networking.check_socket_status(logger)
