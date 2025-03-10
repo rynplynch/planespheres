b8f9f8ce1413e6d1dc75647ca93b63d9415bf5d6
Ryan Lynch <rynplynch@gmail.com>
2025-03-09 23:48:30 -0700

    Logic for create_match RPC call.

--- game/scripts/singletons/rpc.gd
+++ game/scripts/singletons/rpc.gd
@@ -2,6 +2,30 @@ extends Node
 
 # Ask the server to create a match for the user
 func create_match(logger : Control) -> bool:
+	# Everything we need to make the rpc call
+	var client : NakamaClient = Networking._client
+	var session : NakamaSession = Networking._session
+	# The name the create_match function is registered with on the server
+	var rpc_name : String = "create_matchaa"
+	
+	# Check to make sure we can even make the call
+	if await _is_ready(client, session, logger):
+		# Let the user know we are performing work
+		logger.text = "Attempting world creation...\n"
+		
+		# Attempt to call the rpc function
+		var response : NakamaAPI.ApiRpc = await client.rpc_async(session, rpc_name)
+		
+		# Handle any errors
+		if response.is_exception():
+			# show the user the error
+			logger.text = logger.text + response.exception.message + '\n'
+			return false
+		
+		logger.text = logger.text + "World created!\n"
+		return true
+	
+	# We can't make the call, Networking singleton will log info for user
 	return false
 
 # Ask the server to return a list of matches, based upon our request
